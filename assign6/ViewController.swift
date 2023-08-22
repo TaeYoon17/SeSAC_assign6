64f98e27ada0f2cd0f30207221be73034f495088
TaeYoon17 <kim0517@hotmail.com>
2023-08-22 16:29:38 +0900

    PRPRPR

--- assign6/ViewController.swift
+++ assign6/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("prprpr")
         // Do any additional setup after loading the view.
     }
 
